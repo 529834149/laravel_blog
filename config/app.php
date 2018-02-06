9e5a2466dd8d519bbc6ba382c49b52dabc8b5c21
chenbaojin <529834149@qq.com>
2018-02-06 11:12:56 +0800

    no message

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Jenssegers\Mongodb\MongodbServiceProvider::class,
+//        Jenssegers\Mongodb\MongodbServiceProvider::class,
 
     ],
 
@@ -226,7 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Moloquent' => Jenssegers\Mongodb\Eloquent\Model::class,
+//        'Moloquent' => Jenssegers\Mongodb\Eloquent\Model::class,
 
     ],
 
