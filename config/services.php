624cd63aa1c69dd99b81a61704145bac70e15e4c
chenbaojin <529834149@qq.com>
2018-02-28 17:15:32 +0800

    no message

--- config/services.php
+++ config/services.php
@@ -34,5 +34,14 @@ return [
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
+    'weixin' => [
+        'client_id'     => env('WEIXIN_KEY'),
+        'client_secret' => env('WEIXIN_SECRET'),
+        'redirect'      => env('WEIXIN_REDIRECT_URI'),
+
+        # 这一行配置非常重要，必须要写成这个地址。
+        'auth_base_uri' => 'https://open.weixin.qq.com/connect/qrconnect',
+     ],
 
 ];
+
