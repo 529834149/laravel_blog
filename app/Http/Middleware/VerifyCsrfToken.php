6710ba1612cfec1e00a31b6d41d6f37eef579ec2
chenbaojin <529834149@qq.com>
2018-02-28 11:31:37 +0800

    no message

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends BaseVerifier
      */
     protected $except = [
        '*update_click*',
-        '*readability*'
+        '*readability*',
+        '*webchat*'
     ];
 }
