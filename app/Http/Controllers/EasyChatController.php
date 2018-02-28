892e531850d941f22cfb5edf897a932b516dabfc
chenbaojin <529834149@qq.com>
2018-02-28 11:55:28 +0800

    no message

--- app/Http/Controllers/EasyChatController.php
+++ app/Http/Controllers/EasyChatController.php
@@ -6,11 +6,19 @@ use App\Http\Requests;
 use App\Model\Article;
 use App\Model\Categories;
 use DB;
+use Log;
 class EasyChatController extends Controller
 {
     public function wx(Request $request)
     {
-        var_dump($request->all());
+       Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+
+        $app = app('wechat.official_account');
+        $app->server->push(function($message){
+            return "欢迎关注 overtrue！";
+        });
+
+        return $app->server->serve();
     }
     /**
      * 以下方法主要处理分类信息
