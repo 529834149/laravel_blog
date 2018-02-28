f17c5e0bc27a27f1ae2d21d8f2108861ee4f92a6
chenbaojin <529834149@qq.com>
2018-02-28 17:40:53 +0800

    no message

--- config/app.php
+++ config/app.php
@@ -179,7 +179,6 @@ return [
 //        Jenssegers\Mongodb\MongodbServiceProvider::class,
         Encore\Admin\Providers\AdminServiceProvider::class,
          Overtrue\LaravelWeChat\ServiceProvider::class,
-         \SocialiteProviders\Manager\ServiceProvider::class,
     ],
 
     /*
@@ -230,7 +229,6 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 //        'Moloquent' => Jenssegers\Mongodb\Eloquent\Model::class,
         'EasyWeChat' => Overtrue\LaravelWeChat\Facade::class,
-        'Socialite' => Laravel\Socialite\Facades\Socialite::class,  
     ],
 
 ];
