c2e7eb4a9deba0692f2668012c66e87a4eaf80f1
chenbaojin <529834149@qq.com>
2018-03-06 11:06:56 +0800

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,78 +31,76 @@
                  @yield('content')
                 <!--@include('layouts.item')-->
                 @include('layouts._left')
-     <!--推荐的文章-->
-    <div class="blog-sidebar margin-b-30">
-        <div class="blog-sidebar-heading">
-            <i class="icon ion-trophy"></i>
-            <h4 class="blog-sidebar-heading-title">大家都在看</h4>
-        </div>
+                <!--推荐的文章-->
+               <div class="blog-sidebar margin-b-30">
+                   <div class="blog-sidebar-heading">
+                       <i class="icon ion-trophy"></i>
+                       <h4 class="blog-sidebar-heading-title">大家都在看</h4>
+                   </div>
 
-        <div class="blog-sidebar-content">
-            <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: red;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">1</span>将二维数组按着时间进行分组</a></h5>
-            <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: red;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">2</span>将二维数组按着时间进行分组</a></h5>
-            <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: red;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">3</span>将二维数组按着时间进行分组</a></h5>
-            <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: #ccc;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">4</span>将二维数组按着时间进行分组</a></h5>
-            <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: #ccc;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">5</span>将二维数组按着时间进行分组</a></h5>
-            <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: #ccc;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">6</span>将二维数组按着时间进行分组</a></h5>
-            <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: #ccc;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">7</span>将二维数组按着时间进行分组</a></h5>
-            <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: #ccc;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">8</span>将二维数组按着时间进行分组</a></h5>
+                   <div class="blog-sidebar-content">
+                       <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: red;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">1</span>将二维数组按着时间进行分组</a></h5>
+                       <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: red;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">2</span>将二维数组按着时间进行分组</a></h5>
+                       <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: red;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">3</span>将二维数组按着时间进行分组</a></h5>
+                       <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: #ccc;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">4</span>将二维数组按着时间进行分组</a></h5>
+                       <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: #ccc;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">5</span>将二维数组按着时间进行分组</a></h5>
+                       <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: #ccc;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">6</span>将二维数组按着时间进行分组</a></h5>
+                       <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: #ccc;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">7</span>将二维数组按着时间进行分组</a></h5>
+                       <h5 class="timeline-v2-news-title"><a href="http://wanli.org/article/5"><span style="background-color: #ccc;margin: 0 5px 0 0;padding: 0 5px;    border-radius: 2px;font-size: 14px;color:beige;">8</span>将二维数组按着时间进行分组</a></h5>
 
 
-        
-        </div>
-        
-    </div>                        
-    <div class="blog-sidebar margin-b-30">
-        <div class="blog-sidebar-heading">
-            <i class="blog-sidebar-heading-icon fa fa-link"></i>
-            <h4 class="blog-sidebar-heading-title">友情链接</h4>
-        </div>
 
-        <div class="blog-sidebar-content">
-            @include('layouts._link')
-        
-        </div>
-        
-    </div>  
-   
-    <!--归档栏目-->
-    <div class="blog-sidebar margin-b-30">
-       <div class="blog-sidebar-heading">
-           <i class="icon ion-clock"></i>
-           <h4 class="blog-sidebar-heading-title">归档</h4>
-       </div>
-       <div class="blog-sidebar-content">
-         <h3>2018</h3>
-         <ul style="list-style: none;">
-             <li><i class="icon ion-ios-plus-outline"></i>12月<span style="color: #999;margin-left:15px; ">2篇</span></li>
-             <li><i class="icon ion-ios-plus"></i>11月<span style="color: #999;margin-left:15px;">2篇</span></li>
-             <li><i class="icon ion-ios-minus-outline"></i>10月<span style="color: #999;margin-left:15px; ">2篇</span></li>
-         </ul>
-         <h3>2017</h3>
-         <ul style="list-style: none;">
-             <li>12月<span style="color: #999;margin-left:15px; ">2篇</span></li>
-             <li>11月<span style="color: #999;margin-left:15px;">2篇</span></li>
-             <li>10月<span style="color: #999;margin-left:15px; ">2篇</span></li>
-         </ul>
-<!--           <li>12月<a href="//zhangge.net/5115.html" target="_blank">利用HSTS安全协议柔性解决全站HTTPS的兼容性问题</a></li>
-             <li>11月<a href="//zhangge.net/5115.html" target="_blank">利用HSTS安全协议柔性解决全站HTTPS的兼容性问题</a></li>
-             <li>10月<a href="//zhangge.net/5115.html" target="_blank">利用HSTS安全协议柔性解决全站HTTPS的兼容性问题</a></li>-->
+                   </div>
 
-       </div>
-        
-    </div>
-    
-     <div class="blog-sidebar margin-b-30">
-        <div class="blog-sidebar-heading">
-            <i class="icon ion-calendar"></i>
-            <h4 class="blog-sidebar-heading-title">广告栏</h4>
-        </div>
-        <div class="blog-sidebar-content">
-            @include('layouts._advertisement')
-        </div>
-    </div>          
-    </div>
+               </div>                        
+
+
+               <!--归档栏目-->
+               <div class="blog-sidebar margin-b-30">
+                  <div class="blog-sidebar-heading">
+                      <i class="icon ion-clock"></i>
+                      <h4 class="blog-sidebar-heading-title">归档</h4>
+                  </div>
+                  <div class="blog-sidebar-content">
+                    <h3>2018</h3>
+                    <ul style="list-style: none;">
+                        <li><i class="icon ion-ios-plus-outline"></i>12月<span style="color: #999;margin-left:15px; ">2篇</span></li>
+                        <li><i class="icon ion-ios-plus"></i>11月<span style="color: #999;margin-left:15px;">2篇</span></li>
+                        <li><i class="icon ion-ios-minus-outline"></i>10月<span style="color: #999;margin-left:15px; ">2篇</span></li>
+                    </ul>
+                    <h3>2017</h3>
+                    <ul style="list-style: none;">
+                        <li>12月<span style="color: #999;margin-left:15px; ">2篇</span></li>
+                        <li>11月<span style="color: #999;margin-left:15px;">2篇</span></li>
+                        <li>10月<span style="color: #999;margin-left:15px; ">2篇</span></li>
+                    </ul>
+           <!--           <li>12月<a href="//zhangge.net/5115.html" target="_blank">利用HSTS安全协议柔性解决全站HTTPS的兼容性问题</a></li>
+                        <li>11月<a href="//zhangge.net/5115.html" target="_blank">利用HSTS安全协议柔性解决全站HTTPS的兼容性问题</a></li>
+                        <li>10月<a href="//zhangge.net/5115.html" target="_blank">利用HSTS安全协议柔性解决全站HTTPS的兼容性问题</a></li>-->
+
+                  </div>
+
+               </div>
+               <div class="blog-sidebar margin-b-30">
+                   <div class="blog-sidebar-heading">
+                       <i class="blog-sidebar-heading-icon fa fa-link"></i>
+                       <h4 class="blog-sidebar-heading-title">友情链接</h4>
+                   </div>
+                   <div class="blog-sidebar-content">
+                       @include('layouts._link')
+                   </div>
+               </div>  
+               <div class="blog-sidebar margin-b-30">
+                   <div class="blog-sidebar-heading">
+                       <i class="icon ion-calendar"></i>
+                       <h4 class="blog-sidebar-heading-title">广告栏</h4>
+                   </div>
+                   <div class="blog-sidebar-content">
+                       @include('layouts._advertisement')
+                   </div>
+               </div>  
+
+               </div>
             </div>
         </div>
     </div>
