76921362660ff86f6355f1b3c37ed574aeedde4d
chenbaojin <529834149@qq.com>
2018-03-14 10:00:04 +0800

    no message

--- resources/views/layouts/_advertisement.blade.php
+++ resources/views/layouts/_advertisement.blade.php
@@ -1,1 +1,31 @@
-<span style="color:red">广告首年5元</span>
+<div id="myCarousel" class="carousel slide">
+    
+    <!-- 轮播（Carousel）指标 -->
+    <ol class="carousel-indicators" style="bottom:0">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>   
+    <!-- 轮播（Carousel）项目 -->
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="/public/default/images/01d221553349ec000000c50017264c.jpg" alt="First slide">
+            <!--<div class="carousel-caption"></div>-->
+        </div>
+        <div class="item">
+            <img src="/public/default/images/0036wiakzy6GDLeTzKtd5&690.jpg" alt="Second slide">
+            <!--<div class="carousel-caption"></div>-->
+        </div>
+        <div class="item">
+            <img src="/public/default/images/14048844265687.jpg" alt="Third slide">
+            <!--<div class="carousel-caption"></div>-->
+        </div>
+    </div>
+    <!-- 轮播（Carousel）导航 -->
+<!--    <a class="carousel-control left" href="#myCarousel" 
+        data-slide="prev">&lsaquo;
+    </a>
+    <a class="carousel-control right" href="#myCarousel" 
+        data-slide="next">&rsaquo;
+    </a>-->
+</div>
