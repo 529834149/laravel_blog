84d317000f33cb9f2ca2a5dcc707357e1aaafbbb
chenbaojin <529834149@qq.com>
2018-03-14 11:26:22 +0800

    no message

--- resources/views/layouts/_left.blade.php
+++ resources/views/layouts/_left.blade.php
@@ -2,7 +2,7 @@
 <div class="blog-sidebar margin-b-30">
     <div class="blog-sidebar-heading">
         <i class="blog-sidebar-heading-icon fa fa-fire"></i>
-        <h4 class="blog-sidebar-heading-title">热门文章</h4>
+        <h4 class="blog-sidebar-heading-title">时间轴</h4>
     </div>
     <div class="blog-sidebar-content">
         <ul class="timeline-v2">
