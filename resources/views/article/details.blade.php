974617e9b45f8e18eea9bf5a45ad6057d37172e0
chenbaojin <529834149@qq.com>
2018-04-25 17:29:32 +0800

    no message

--- resources/views/article/details.blade.php
+++ resources/views/article/details.blade.php
@@ -254,6 +254,7 @@
             dataType:'jsonp',
             jsonp: 'callback',
             success:function(msg){ 
+                console.log(msg)
                 if(msg.code == 'A0200'){ 
                     wx.config({
                         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
