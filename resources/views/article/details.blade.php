9be1fc4eb493c3ac0d6608cc47d42a0d8310dae8
chenbaojin <529834149@qq.com>
2018-04-25 17:21:10 +0800

    no message

--- resources/views/article/details.blade.php
+++ resources/views/article/details.blade.php
@@ -256,7 +256,7 @@
             success:function(msg){ 
                 if(msg.code == 'A0200'){ 
                     wx.config({
-                        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                         appId: msg.data.appid, // 必填，公众号的唯一标识
                         timestamp: msg.data.timestamp, // 必填，生成签名的时间戳
                         nonceStr: msg.data.noncestr, // 必填，生成签名的随机串
