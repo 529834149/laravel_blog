63bfd7975e29316307f7a53a421dc90ad4cb6aa3
chenbaojin <529834149@qq.com>
2018-05-25 17:24:41 +0800

    no message

---
+++ resources/views/special/list.blade.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="_token" content="ir5yoi0GDipK8RGb151VwfwNf8zjSWllWOE6unMC">
+    <meta name="csrf-token" content="ir5yoi0GDipK8RGb151VwfwNf8zjSWllWOE6unMC">
+    <title></title>
+    <link rel="stylesheet" href="/public/special/css/app.css">
+    <link rel="stylesheet" href="/public/special/css/main.css">
+    <link href="/public/special/css/semantic.min.css" rel="stylesheet">
+    <script src="/public/special/js/jquery.min.js"></script>
+    <script src="/public/special/js/semantic.min.js"></script>
+    <link href="/public/special/css/swiper.min.css" rel="stylesheet">
+    <link href="//at.alicdn.com/t/font_597522_al1b2ouz6m3g14i.css" rel="stylesheet">
+    <link href="https://cdn.bootcss.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet">
+   
+    <style>
+        body {
+            background-color: #FFFFFF;
+        }
+
+        .ui.menu .item img.logo {
+            margin-right: 1.5em;
+        }
+
+        .main.container {
+            margin-top: 7em;
+        }
+
+        .wireframe {
+            margin-top: 2em;
+        }
+
+        .ui.footer.segment {
+            margin: 5em 0em 0em;
+            padding: 5em 0em;
+        }
+
+        .swiper-container {
+            width: 100%;
+            height: 100%;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .swiper-slide {
+            text-align: center;
+            font-size: 18px;
+            background: #fff;
+            /* Center slide text vertically */
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: -webkit-flex;
+            display: flex;
+            -webkit-box-pack: center;
+            -ms-flex-pack: center;
+            -webkit-justify-content: center;
+            justify-content: center;
+            -webkit-box-align: center;
+            -ms-flex-align: center;
+            -webkit-align-items: center;
+            align-items: center;
+        }
+
+        .swiper-slide img:hover {
+            cursor: pointer;
+        }
+
+        .fluid.card .image {
+            height: 250px;
+            background-image: url('/public/special/picture/xlaz6z2hsw.png');
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center center;
+        }
+
+        /*.swiper-pagination-bullet-active {
+            background-color: #009c95;!important;
+        }*/
+    </style>
+    </head>
+<body>
+<div class="ui main large stackable fixed inverted menu">
+    <div class="ui container">
+        <a href="/" class="header item">
+            <img class="logo" src="/public/special/picture/logo.png">
+            主页
+        </a>
+        <a href="/special" class="item">专辑</a>e
+        <a href="/rank" class="item">发现</a>
+        <div class="ui simple dropdown item right">
+            <img class="ui avatar image" alt="Destiny"
+                 src="/public/special/picture/4430_1481108240.jpg"> Destiny
+            <i class="dropdown icon"></i>
+            <div class="menu">
+                <a class="item" href="/user"><i class="icon ion-android-person"></i> 个人中心</a>
+                <a class="item" href="/settings"><i class="icon ion-settings"></i> 账户设置</a>
+                <a class="item" href="#"><i class="icon ion-close-round"></i> 退出登录</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="content-wrapper">
+        <div class="ui secondary segment">
+        <div class="ui main container" style="height: 400px;margin-top: 3em">
+            <div class="swiper-container ui segment">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <img src="/public/special/picture/mbe.png">
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="/public/special/picture/mbe.png">
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="/public/special/picture/meteor.png">
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="/public/special/picture/meteor.png">
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="/public/special/picture/meteor.png">
+                    </div>
+
+                </div>
+                <!-- Add Pagination -->
+                <div class="swiper-pagination"></div>
+                <!-- Add Arrows -->
+                <!-- <div class="swiper-button-next"></div> -->
+                <!-- <div class="swiper-button-prev"></div> -->
+            </div>
+        </div>
+    </div>
+    <div class="ui container">
+        <div class="ui two column stackable grid link cards">
+            <div class="column">
+                <div class="ui fluid card">
+                    <div class="ui fluid image">
+                        <div class="ui black ribbon label"><i class="icon ion-ios-star"></i> 精选</div>
+                        <a class="image"></a>
+                    </div>
+                    <div class="content">
+                        <a class="header">丹尼尔 路易斯111</a>
+                    </div>
+                    <div class="extra content">
+                        <div class="left floated author">
+                            <img class="ui avatar image"
+                                 src="https://dn-phphub.qbox.me/uploads/images/201709/10/4430/xlAZ6z2HsW.png"> Matt
+                        </div>
+                        <span class="right floated">
+                        <span class="like"><i class="icon ion-heart"></i> 喜欢(30)</span>
+                        <span class="star"><i class="icon ion-ios-eye-outline"></i> 浏览(30) </span>
+                        <span class="wait"><i class="icon ion-ios-timer-outline"></i>发布时间:2017-10-10 </span>
+                    </span>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="ui fluid card">
+                    <div class="ui fluid image">
+                        <div class="ui black ribbon label"><i class="icon ion-ios-star"></i> 精选</div>
+                        <a class="image"></a>
+                    </div>
+                    <div class="content">
+                        <a class="header">丹尼尔 路易斯</a>
+                    </div>
+                    <div class="extra content">
+                        <div class="left floated author">
+                            <img class="ui avatar image"
+                                 src="https://dn-phphub.qbox.me/uploads/images/201709/10/4430/xlAZ6z2HsW.png"> Matt
+                        </div>
+                        <span class="right floated">
+                         <span class="like"><i class="icon ion-heart"></i> 喜欢(30)</span>
+                        <span class="star"><i class="icon ion-ios-eye-outline"></i> 浏览(30) </span>
+                        <span class="wait"><i class="icon ion-ios-timer-outline"></i>发布时间:2017-10-10 </span>
+                    </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="ui section horizontal divider"><i class="trophy icon"></i></div>
+        <div class="ui grid">
+            <div class="ui column tag labels">
+                <a class="ui teal label">全部</a>
+                <a class="ui label">Laravel</a>
+                <a class="ui label">Laravel</a>
+                <a class="ui label">Laravel</a>
+            </div>
+        </div>
+        <div class="ui three column stackable grid link cards">
+            <div class="column">
+                <div class="ui fluid card">
+                    <a class="image"></a>
+                    <div class="content">
+                        <a class="header">丹尼尔 路易斯</a>
+                    </div>
+                    <div class="extra content">
+                        <div class="left floated author">
+                            <img class="ui avatar image"
+                                 src="https://dn-phphub.qbox.me/uploads/images/201709/10/4430/xlAZ6z2HsW.png"> Matt
+                        </div>
+                        <span class="right floated">
+                         <span class="like"><i class="icon ion-heart"></i> 喜欢(30)</span>
+                        <span class="star"><i class="icon ion-ios-eye-outline"></i> 浏览(30) </span>
+                        <span class="wait"><i class="icon ion-ios-timer-outline"></i>发布时间:2017-10-10 </span>
+                    </span>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="ui fluid card">
+                    <a class="image"></a>
+                    <div class="content">
+                        <a class="header">丹尼尔 路易斯</a>
+                    </div>
+                    <div class="extra content">
+                        <div class="left floated author">
+                            <img class="ui avatar image"
+                                 src="https://dn-phphub.qbox.me/uploads/images/201709/10/4430/xlAZ6z2HsW.png"> Matt
+                        </div>
+                        <span class="right floated">
+                         <span class="like"><i class="icon ion-heart"></i> 喜欢(30)</span>
+                        <span class="star"><i class="icon ion-ios-eye-outline"></i> 浏览(30) </span>
+                        <span class="wait"><i class="icon ion-ios-timer-outline"></i>发布时间:2017-10-10 </span>
+                    </span>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="ui fluid card">
+                    <a class="image"></a>
+                    <div class="content">
+                        <a class="header">丹尼尔 路易斯</a>
+                    </div>
+                    <div class="extra content">
+                        <div class="left floated author">
+                            <img class="ui avatar image"
+                                 src="https://dn-phphub.qbox.me/uploads/images/201709/10/4430/xlAZ6z2HsW.png"> Matt
+                        </div>
+                        <span class="right floated">
+                        <span class="like"><i class="icon ion-heart"></i> 喜欢(30)</span>
+                        <span class="star"><i class="icon ion-ios-eye-outline"></i> 浏览(30) </span>
+                        <span class="wait"><i class="icon ion-ios-timer-outline"></i>发布时间:2017-10-10 </span>
+                    </span>
+                    </div>
+                </div>
+            </div>
+           
+        </div>
+        <div class="ui container">
+            <div class="ui pagination menu">
+                <a class="active item">
+                    1
+                </a>
+                <div class="disabled item">
+                    ...
+                </div>
+                <a class="item">
+                    10
+                </a>
+                <a class="item">
+                    11
+                </a>
+                <a class="item">
+                    12
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="ui inverted vertical footer segment">
+    <div class="ui center aligned container">
+        <div class="ui stackable inverted divided grid">
+            <div class="three wide column">
+                <h4 class="ui inverted header">Group 1</h4>
+                <div class="ui inverted link list">
+                    <a href="#" class="item">Link One</a>
+                    <a href="#" class="item">Link Two</a>
+                    <a href="#" class="item">Link Three</a>
+                    <a href="#" class="item">Link Four</a>
+                </div>
+            </div>
+            <div class="three wide column">
+                <h4 class="ui inverted header">Group 2</h4>
+                <div class="ui inverted link list">
+                    <a href="#" class="item">Link One</a>
+                    <a href="#" class="item">Link Two</a>
+                    <a href="#" class="item">Link Three</a>
+                    <a href="#" class="item">Link Four</a>
+                </div>
+            </div>
+            <div class="three wide column">
+                <h4 class="ui inverted header">Group 3</h4>
+                <div class="ui inverted link list">
+                    <a href="#" class="item">Link One</a>
+                    <a href="#" class="item">Link Two</a>
+                    <a href="#" class="item">Link Three</a>
+                    <a href="#" class="item">Link Four</a>
+                </div>
+            </div>
+            <div class="seven wide column">
+                <h4 class="ui inverted header">Footer Header</h4>
+                <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+            </div>
+        </div>
+        <div class="ui inverted section divider"></div>
+        <img src="picture/logo.png" class="ui centered mini image">
+        <div class="ui horizontal inverted small divided link list">
+            <a class="item" href="#">Site Map</a>
+            <a class="item" href="#">Contact Us</a>
+            <a class="item" href="#">Terms and Conditions</a>
+            <a class="item" href="#">Privacy Policy</a>
+        </div>
+    </div>
+</div>
+</body>
+<script src="/public/special/js/swiper.min.js"></script>
+<script src="/public/special/js/app.js"></script>
+<script src="/public/special/js/main.js"></script>
+<!-- Initialize Swiper -->
+<script>
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        effect: 'fade',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: 2500,
+    });
+</script>
+</html>
