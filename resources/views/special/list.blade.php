947f4e96c86baaf38edbd97f667efd869911d3fb
chenbaojin <529834149@qq.com>
2018-05-29 15:00:19 +0800

    no message

--- resources/views/special/list.blade.php
+++ resources/views/special/list.blade.php
@@ -76,4 +76,89 @@
             @endforeach
         </div>
     </div>
+<script>
+    var divShow = true;  
+    $(".more").click(function(){  
+        if(divShow){
+            $('.more i').removeClass('ion-chevron-down');
+            $('.more i').addClass('ion-chevron-up');
+            $(".label_item").fadeIn(300);
+            divShow = false;  
+        }else{
+            $('.more i').removeClass('ion-chevron-up');
+            $('.more i').addClass('ion-chevron-down');
+            $(".label_item").fadeOut(300);
+            divShow = true;  
+        }
+    })
+    function RiQi(sj){
+       var now = new Date(sj*1000);
+       var   year=now.getFullYear();    
+         var   month=now.getMonth()+1;    
+         var   date=now.getDate();    
+         var   hour=now.getHours();    
+         var   minute=now.getMinutes();    
+         var   second=now.getSeconds();  
+         if(month<10){
+             month = '0'+month;
+         }
+        return   year+"-"+month+"-"+date; 
+//         return   year+"-"+month+"-"+date+"   "+hour+":"+minute+":"+second;    
+        
+   }
+    function items(id){
+        $('.menu_info').css('background-color','')
+        $('#'+id).css('background-color','#00b5ad')
+        $.ajax({
+            type:'get',
+            url: '/documents/'+id,
+            data:{
+                
+            },
+            dataType:'json',
+           beforeSend:function(){
+               
+           },
+            success:function(data){
+                var html='';
+                if(data.meta.code == 200){
+                    var list = data.data;
+                    if(list.length){
+                        list.forEach(function(value, index, array) {
+//                        console.log(array[index]['title']);return false;
+                            html+='<div class="column">'
+                            html+='<div class="ui fluid card">'
+                            html+='<a class="image"></a>'
+                            html+='<div class="content">'
+                            html+='<a class="header">'+array[index]['title']+'</a>'
+                            html+='</div>'
+                            html+='<div class="extra content">'
+                            html+='<div class="left floated author">'
+                            html+='<img class="ui avatar image"'
+                            html+='src="https://dn-phphub.qbox.me/uploads/images/201709/10/4430/xlAZ6z2HsW.png"> Matt'
+                            html+='</div>'
+                            html+='<span class="right floated">'
+                            html+='<span class="like"><i class="icon ion-heart"></i> 喜欢('+array[index]['like']+')</span>'
+                            html+='<span class="star"><i class="icon ion-ios-eye-outline"></i> 浏览('+array[index]['access']+') </span>'
+//                            html+='<span class="wait"><i class="icon ion-ios-timer-outline"></i>发布时间:'+RiQi(array[index]['publish_time'])+' </span>'
+                            html+='</span>'
+                            html+='</div>'
+                            html+='</div>'
+                            html+='</div>'
+                        });
+                        $('.date_list').empty();
+                        $('.date_list').html(html);
+                        return false;
+                    }else{
+                        $('.date_list').empty();
+                        $('.date_list').html('暂无专栏');
+                    }
+                }else{
+                    $('.date_list').empty();
+                    $('.date_list').html('暂无专栏');
+                }
+            }
+        });
+    }
+</script>
 @endsection
