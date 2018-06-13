8d6da8af09873be8d312c16be3ee5fbfbe686e1f
chenbaojin <529834149@qq.com>
2018-06-13 15:15:25 +0800

    no message

--- resources/views/home/topic/list.blade.php
+++ resources/views/home/topic/list.blade.php
@@ -19,27 +19,27 @@
                         <a href="#" title="Laravel 5.4 中文文档">Laravel 5.4 中文文档</a></div>
                 </article>
                 <article class="col-sm-3 series clearfix">
-                    <div class="series-post" style="background-image:url(images/laravel-logo-20160414.jpg)">
+                    <div class="series-post" style="background-image:url(/home/images/laravel-logo-20160414.jpg)">
                         <a href="#" title="Laravel 的 Events(事件) 及 Observers(观察者)">Laravel 的 Events(事件) 及 Observers(观察者)</a></div>
                 </article>
                 <article class="col-sm-3 series clearfix">
-                    <div class="series-post" style="background-image:url(images/intermediate-laravel.jpg)">
+                    <div class="series-post" style="background-image:url(/home/images/intermediate-laravel.jpg)">
                         <a href="#" title="Laravel小技巧">Laravel小技巧</a></div>
                 </article>
                 <article class="col-sm-3 series clearfix">
-                    <div class="series-post" style="background-image:url(images/sublime-text-mastery.jpg)">
+                    <div class="series-post" style="background-image:url(/home/images/sublime-text-mastery.jpg)">
                         <a href="#" title="Sublime Text 技巧">Sublime Text 技巧</a></div>
                 </article>
                 <article class="col-sm-3 series clearfix">
-                    <div class="series-post" style="background-image:url(images/modern-css-workflow.jpg)">
+                    <div class="series-post" style="background-image:url(/home/images/modern-css-workflow.jpg)">
                         <a href="#" title="程序员必备的开发工具">程序员必备的开发工具</a></div>
                 </article>
                 <article class="col-sm-3 series clearfix">
-                    <div class="series-post" style="background-image:url(images/laravel-from-logo.jpg)">
+                    <div class="series-post" style="background-image:url(/home/images/laravel-from-logo.jpg)">
                         <a href="#" title="Laravel 5 开发博客系统教程">Laravel 5 开发博客系统教程</a></div>
                 </article>
                 <article class="col-sm-3 series clearfix">
-                    <div class="series-post" style="background-image:url(images/php.jpg)">
+                    <div class="series-post" style="background-image:url(/home/images/php.jpg)">
                         <a href="#" title="PHP部分功能讲解及源码分析">PHP部分功能讲解及源码分析</a></div>
                 </article>
             </section>
