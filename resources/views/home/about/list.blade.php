eba5deed926ceedf765e3dcc61f188d3a6938255
chenbaojin <529834149@qq.com>
2018-06-20 13:38:16 +0800

    no message

--- resources/views/home/about/list.blade.php
+++ resources/views/home/about/list.blade.php
@@ -3,7 +3,22 @@
 @section('title',' 的个人中心')
 
 @section('content')
+  <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"/>
+  <link rel="stylesheet" href="https://g.alicdn.com/de/prismplayer/2.3.5/skins/default/aliplayer-min.css"/>
+  <script type="text/javascript" src="https://g.alicdn.com/de/prismplayer/2.3.5/aliplayer-min.js"></script>
 <section class="row">
+    <div class="prism-player" id="J_prismPlayer">
+    </div>
+    <script>
+        var player = Aliplayer({
+            id: "J_prismPlayer",
+            autoplay: true,
+            width: "1920px",
+            height: "1280px",
+            vid: "58353d059f204e9b9f1b64774be1e4f2",
+            playauth: "客户调用接口生成",
+        });
+    </script>
     <section id="main" class="col-md-8">
         <article class="well clearfix page" id="post">
             <header class="entry-header">
